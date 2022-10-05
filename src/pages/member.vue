c433588493936d660a14db89335b04bce14c2968
sin392 <sin392@gmail.com>
2022-10-05 15:51:41 +0900

    update member page

--- src/pages/member.vue
+++ src/pages/member.vue
@@ -6,23 +6,23 @@ import PeopleCard from '~/components/people-card'
   <div class="wrapper">
     <PeopleCard
       name="しのりん"
-      hobby="旅行"
-      skill="色々"
-      comment="特になし"
+      hobby="旅行 / 英語 / 個人開発"
+      skill="機械学習 / 英語 / 教育 / WEB / メディア"
+      comment="高専在学中に40カ国に旅行する中で悟りを開きました"
       imgFileName="member-shinorin.webp"
     />
     <PeopleCard
       name="まいん"
-      hobby="カメラ / コーヒー / 読書"
-      skill="機械学習 / ロボティクス / Web / コンテンツツーリズム"
-      comment="特になし"
+      hobby="カメラ / コーヒー / ゲーム / アニメ / 散歩"
+      skill="ロボティクス / 機械学習 / Web / 組み込み / コンテンツツーリズム"
+      comment="ブレンダー勉強中、先生求む"
       imgFileName="member-mine.webp"
     />
     <PeopleCard
       name="ざき"
-      hobby="ゲーム"
-      skill="機械学習 / ロボティクス / Web / コンテンツツーリズム"
-      comment="特になし"
+      hobby="ゲーム / エレキギター / キャンプ / サウナ"
+      skill="機械学習 / 材料 / 自然言語 / 楽器 / デザイン / WEB / 寮生活"
+      comment="肉汁の出るハンバーグをずぼらに作る方法について日夜研究を重ねています"
       imgFileName="member-zaki.webp"
     />
   </div>
