75211f39b939b24fd364d112eaf6b08dc10ef1ec
sin392 <sin392@gmail.com>
2022-10-05 17:16:14 +0900

    update work page

--- src/pages/work.vue
+++ src/pages/work.vue
@@ -1,5 +1,19 @@
 <template>
-  <div></div>
+  <div class="wrapper"><div>工事中</div></div>
 </template>
 
-<style scoped></style>
+<style scoped>
+.wrapper {
+  width: 60%;
+  height: 80%;
+  margin: auto;
+  box-shadow: 0 0 5px darkgray;
+  padding: 20px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  font-size: xx-large;
+  font-weight: bold;
+}
+</style>
