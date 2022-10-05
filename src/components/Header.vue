e5a3846657a473f11cf36ab814441d407e609ee2
sin392 <sin392@gmail.com>
2022-10-05 10:31:43 +0900

    add header navigator

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,16 +1,47 @@
 <template>
   <header>
-    <div>Header</div>
+    <div id="header-title">Header</div>
+    <nav>
+      <ul>
+        <li>About</li>
+        <li>Work</li>
+        <li>Contact</li>
+      </ul>
+    </nav>
   </header>
 </template>
 
 <style scoped>
 header {
   height: 80px;
-  line-height: 80px;
-  padding: 0 20px;
+  padding: 0 40px;
   background-color: var(--main-bg-color);
   color: var(--font-color-on-main-bg);
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  text-shadow: 1px 1px 2px gray;
+}
+#header-title {
   font-size: xx-large;
+}
+ul {
+  padding: 0;
+  margin: 0;
+  display: flex;
+  gap: 10px;
+}
+li {
+  height: fit-content !important;
+  font-size: x-large;
+  list-style-type: none;
+}
+#header-title,
+li {
+  cursor: pointer;
+}
+#header-title:hover,
+li:hover {
+  text-shadow: 1px 1px 2px black;
 }
 </style>
