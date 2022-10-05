8c36e4461f98990b57a0641056efb94bbd458c81
sin392 <sin392@gmail.com>
2022-10-05 15:43:12 +0900

    add content to member page

---
+++ src/components/people-card.vue
@@ -0,0 +1,72 @@
+<script setup lang="ts">
+interface PeopleCardProps {
+  name: string
+  hobby: string
+  skill: string
+  comment: string
+  imgFileName: string
+}
+
+const props = defineProps<PeopleCardProps>()
+// NOTE: imgSrcをそのまま:srcに渡したり、require(imgSrc)とするのはうまくいかなかった
+const imgSrc = require(`~/assets/${props.imgFileName}`)
+</script>
+
+<template>
+  <div class="people-card">
+    <div class="people-card-header">
+      <img :src="imgSrc" alt="画像" />
+    </div>
+    <div class="people-card-content">
+      <p class="name">{{ name }}</p>
+      <div class="info-wrapper">
+        <div><span class="label">趣味：</span>{{ hobby }}</div>
+        <div><span class="label">得意分野：</span>{{ skill }}</div>
+        <div><span class="label">ひとこと：</span>{{ comment }}</div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.people-card {
+  display: flex;
+  width: 1000px;
+  gap: 20px;
+}
+.people-card-header {
+  width: 180px;
+  height: 180px;
+  display: flex;
+}
+img {
+  margin: auto;
+  max-width: 100%;
+  border-radius: 50%;
+  box-shadow: 0 0 10px darkgray;
+  /* 画像のドラッグ,選択,ダウンロードを禁止 */
+  pointer-events: none;
+  user-select: none;
+}
+.people-card-content {
+  flex-grow: 1;
+  padding: 20px 30px;
+  border-radius: 25px;
+  box-shadow: 0 0 5px darkgray;
+}
+.name {
+  font-size: x-large;
+  font-weight: bold;
+  margin-bottom: 10px;
+}
+.info-wrapper {
+  display: flex;
+  flex-direction: column;
+  gap: 5px;
+  font-size: large;
+}
+.label {
+  display: inline-block;
+  min-width: 6rem;
+}
+</style>
--- src/pages/member.vue
+++ src/pages/member.vue
@@ -1,5 +1,41 @@
+<script setup>
+import PeopleCard from '~/components/people-card'
+</script>
+
 <template>
-  <div></div>
+  <div class="wrapper">
+    <PeopleCard
+      name="しのりん"
+      hobby="旅行"
+      skill="色々"
+      comment="特になし"
+      imgFileName="member-shinorin.webp"
+    />
+    <PeopleCard
+      name="まいん"
+      hobby="カメラ / コーヒー / 読書"
+      skill="機械学習 / ロボティクス / Web / コンテンツツーリズム"
+      comment="特になし"
+      imgFileName="member-mine.webp"
+    />
+    <PeopleCard
+      name="ざき"
+      hobby="ゲーム"
+      skill="機械学習 / ロボティクス / Web / コンテンツツーリズム"
+      comment="特になし"
+      imgFileName="member-zaki.webp"
+    />
+  </div>
 </template>
 
-<style scoped></style>
+<style scoped>
+.wrapper {
+  width: 100%;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  gap: 20px;
+}
+</style>
