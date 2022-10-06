412c0aac91bd5d570ea914d817bf781feb16944b
sin392 <sin392@gmail.com>
2022-10-07 00:28:45 +0900

    extract same string as const

---
+++ src/consts/url.ts
@@ -0,0 +1,4 @@
+export const TWITTER_URL = 'https://twitter.com/KosenSparkle'
+export const YOUTUBE_URL =
+  'https://www.youtube.com/channel/UC6f6GjgVmP55cvLZ2T7FpfQ'
+export const NOTE_URL = 'https://note.com/kosen_sparkle'
