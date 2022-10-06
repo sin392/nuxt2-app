c420f33afe419183647efa11b632df1f61ed52a6
sin392 <sin392@gmail.com>
2022-10-06 14:54:21 +0900

    add .env for SLACK_WEBHOOK_URL

--- netlify/functions/slack.js
+++ netlify/functions/slack.js
@@ -1,16 +1,12 @@
 // ref: https://qiita.com/tokimeki40/items/72897c700693464bc15b
 import fetch from 'node-fetch'
 
-// TODO: envに移行
-const SLACK_WEBHOOK_URL =
-  'https://hooks.slack.com/services/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'
-
 exports.handler = async (event) => {
   if (event.httpMethod !== 'POST') {
     return { statusCode: 405, body: 'Method Not Allowed' }
   }
 
-  return await fetch(SLACK_WEBHOOK_URL, {
+  return await fetch(process.env.SLACK_WEBHOOK_URL, {
     headers: {
       'content-type': 'application/json',
     },
