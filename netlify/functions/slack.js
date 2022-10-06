7562bf809b4904516e3c7dc401723713c5deb3a3
sin392 <sin392@gmail.com>
2022-10-06 15:53:40 +0900

    update notification content

--- netlify/functions/slack.js
+++ netlify/functions/slack.js
@@ -1,17 +1,23 @@
 // ref: https://qiita.com/tokimeki40/items/72897c700693464bc15b
 import fetch from 'node-fetch'
 
-exports.handler = async (event) => {
+exports.handler = async (event, context) => {
   if (event.httpMethod !== 'POST') {
     return { statusCode: 405, body: 'Method Not Allowed' }
   }
+  const payload = JSON.parse(event.body)
+  console.log('payload', payload)
+  console.log('context', context)
+  const state = payload.state
+  const branch = payload.branch
+  const title = payload.title
 
   return await fetch(process.env.SLACK_WEBHOOK_URL, {
     headers: {
       'content-type': 'application/json',
     },
     method: 'POST',
-    body: JSON.stringify({ text: `デプロイが成功しました` }),
+    body: JSON.stringify({ text: `[${branch}] (${title}) site is ${state}` }),
   })
     .then(() => ({
       statusCode: 200,
