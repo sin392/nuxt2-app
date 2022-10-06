0af82bcf88c0d68527e9ae4a0495df8d9defdc2e
sin392 <sin392@gmail.com>
2022-10-06 15:17:11 +0900

    change imgFileName to img-file-name

--- src/pages/member.vue
+++ src/pages/member.vue
@@ -9,21 +9,21 @@ import PeopleCard from '~/components/people-card'
       hobby="旅行 / 英語 / 個人開発"
       skill="機械学習 / 英語 / 教育 / WEB / メディア"
       comment="高専在学中に40カ国に旅行する中で悟りを開きました"
-      imgFileName="member-shinorin-256.webp"
+      img-file-name="member-shinorin-256.webp"
     />
     <PeopleCard
       name="まいん"
       hobby="カメラ / コーヒー / ゲーム / アニメ / 散歩"
       skill="ロボティクス / 機械学習 / Web / 組み込み / コンテンツツーリズム"
       comment="ブレンダー勉強中、先生求む"
-      imgFileName="member-mine-256.webp"
+      img-file-name="member-mine-256.webp"
     />
     <PeopleCard
       name="ざき"
       hobby="ゲーム / エレキギター / キャンプ / サウナ"
       skill="機械学習 / 材料 / 自然言語 / 楽器 / デザイン / WEB / 寮生活"
       comment="肉汁の出るハンバーグをずぼらに作る方法について日夜研究を重ねています"
-      imgFileName="member-zaki-256.webp"
+      img-file-name="member-zaki-256.webp"
     />
   </div>
 </template>
